4e6f3a14554d6565e4a9b0cf48351b16b8540860
Nathan Witmer <nwitmer@gmail.com>
2012-07-24 22:36:04 -0600

    Render app list with mustache template from js objects

--- public/app.js
+++ public/app.js
@@ -1,3 +1,11 @@
 $(function() {
-  console.log("hello world");
+  var apps = [
+    { name: 'main_site', 'running?': true },
+    { name: 'admin_app', 'stopped?': true },
+    { name: 'resque_workers', 'disabled?': true }
+  ];
+  var template = $('#app-template');
+  _.each(apps, function(app) {
+    $('#apps').append( template.mustache(app) );
+  });
 });
